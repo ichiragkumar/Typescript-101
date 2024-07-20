7b2d192a100ec91b64b5f9891dfa3e05d88fbc85
ichiragkumar <chiragroy3030@gmail.com>
2024-07-21 01:40:05 +0530

    objects in typescript

---
+++ src/objectsintypescript.ts
@@ -0,0 +1,37 @@
+
+
+type UserProfile = {
+    height:number;
+    weight:number;
+    gender:boolean;
+}
+
+
+const newUerProfiler:UserProfile = {
+    height:190,
+    weight:90,
+    gender:true,
+}
+
+console.log(newUerProfiler);
+
+
+// make optional object member
+
+
+type txnData =  {
+    amount:number,
+    address:string,
+    name:string,
+    email?:string,
+}
+
+
+const newTxnData:txnData = {
+    amount:100,
+    address:"123 Main Street",
+    name:"John Doe",
+    email:"johndoe@gmail.com",
+}
+console.log(newTxnData);
+
