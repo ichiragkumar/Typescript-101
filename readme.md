7168a4e63f25070719e717e0a2d6623689de1484
ichiragkumar <chiragroy3030@gmail.com>
2024-07-21 01:00:34 +0530

    added more types funciton in typescript

---
+++ readme.md
@@ -0,0 +1,6 @@
+
+1. index.js Baisc
+
+2. learn Types with Basic example
+
+3. Types wtih Function paramets and function return
--- src/typefunction.ts
+++ src/typefunction.ts
@@ -1,13 +1,24 @@
 type checkResponse = (req:number, res:number) => {};
 
-
-
 const saveuser:checkResponse = (req, res) =>{
     return {
         name:req,
         age:res
     }
 }
-
 const resutl = saveuser(10, 20)
 console.log(resutl)
+
+
+type erroerChecker = (req:string, res:string) => {}
+
+const getError:erroerChecker = (req="default 1", res="default 2") =>{
+    return {
+        error:req,
+        message:res,
+    }
+}
+
+const errorResult = getError("server error", "server logs")
+console.log(errorResult);
+
