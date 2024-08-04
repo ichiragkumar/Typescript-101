e533378713c007e6a9ba06bd5352534f5bb4ed1b
CHIRAG KUMAR <chiragroy3030@gmail.com>
2024-08-04 23:26:36 +0530

    make whole object readonly

--- src/readOnlytypes.ts
+++ src/readOnlytypes.ts
@@ -13,3 +13,18 @@ const newUser1: rootUser = {
 };
 
 console.log(newUser1);
+
+//TODO: 2. Make the whole object readonly
+type rootUser2 = {
+  name: string;
+  age: number;
+  gender: boolean;
+};
+
+const newUser2: Readonly<rootUser2> = {
+  name: "chirag",
+  age: 32,
+  gender: true,
+};
+
+console.log(newUser2);
