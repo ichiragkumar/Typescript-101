f8e861ad59c97e491da1b43808e95eedf4737c2e
CHIRAG KUMAR <chiragroy3030@gmail.com>
2024-08-04 23:05:18 +0530

    implemented PICK api in typescript

--- src/AdvanceTypes.ts
+++ src/AdvanceTypes.ts
@@ -1,11 +1,48 @@
-interface Usertwo {
+interface UserDB {
+  id: string;
+  email: string;
+  password: string;
   name: string;
   age: number;
 }
 
-function someofAge(user1: Usertwo, user2: Usertwo) {
-  return user1.age + user2.age;
+// whenever you do a database call , specially in this case you will get this type
+// you have the object in the pre-defined shape
+const proUser: UserDB = findOne({ email: "test@gmail.com" });
+
+//TODO:  given you can only change
+// 1. name
+// 2. age
+// 3. password
+
+// option 1
+// This is bad, because if you have to change the type of the field if any time,
+// you have to changet the type of the whole object or UpdateProps
+interface UpdateUserProps {
+  name: string;
+  age: number;
+  password: string;
 }
 
-const result1 = someofAge({ name: "John", age: 20 }, { name: "Jane", age: 30 });
-console.log(result1);
+function updateUser(updatedProps: UpdateUserProps) {}
+
+// Ideal options is use Pick
+
+type UpdateProps = Pick<UserDB, "name" | "age" | "password">;
+function updateUser2(updatedProps: UpdateProps) {}
+
+interface TransactionDetails {
+  amount: number;
+  date: Date;
+  from: string;
+  To: string;
+  transactionHash: string;
+  message: string;
+  status: Boolean;
+}
+
+type Txns = Pick<
+  TransactionDetails,
+  "from" | "To" | "transactionHash" | "message" | "status"
+>;
+function validateTransaction() {}
